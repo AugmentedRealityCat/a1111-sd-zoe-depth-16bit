9f46f936d52d1c216751b7ccbffd9ab4fd122f5e
AugmentedRealityCat <114955776+AugmentedRealityCat@users.noreply.github.com>
2023-07-24 05:25:21 -0400

    Update gradio_depth_pred.py to support 16 bit PNG output
    Modified using the code written by Ratinod over here:
    https://github.com/sanmeow/a1111-sd-zoe-depth/issues/2#issuecomment-1500460852

--- gradio_depth_pred.py
+++ gradio_depth_pred.py
@@ -23,10 +23,27 @@ def create_demo(model):
     def on_submit(image):
 
         depth = predict_depth(model, image)
-        colored_depth = colorize(depth, cmap='gray_r')
+        #colored_depth = colorize(depth, cmap='gray_r')
         tmp = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
-        raw_depth = Image.fromarray((depth*256).astype('uint16'))
+        #raw_depth = Image.fromarray((depth*256).astype('uint16'))
+        raw_depth = Image.fromarray((65535*(depth - depth.min())/depth.ptp()).astype('uint16'), mode="I;16")
+        width, height = raw_depth.size
+        temp_image = Image.new('I;16', (width, height), (65535))
+        buffer1 = np.asarray(raw_depth)
+        buffer2 = np.asarray(temp_image)
+        buffer3 = buffer2 - buffer1
+        raw_depth = Image.fromarray(buffer3)
+        colored_depth = Image.fromarray((255*(depth - depth.min())/depth.ptp()).astype('uint8'), mode="L")
+        temp_image = Image.new('L', (width, height), (255))
+        buffer1 = np.asarray(colored_depth)
+        buffer2 = np.asarray(temp_image)
+        buffer3 = buffer2 - buffer1
+        colored_depth = Image.fromarray(buffer3)
         raw_depth.save(tmp.name)
+        del buffer1
+        del buffer2
+        del buffer3
+        del temp_image
         del depth
         del raw_depth
         torch.cuda.empty_cache()
